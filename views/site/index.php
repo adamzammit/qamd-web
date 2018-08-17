4515a353a536e66b7f337da5365274a53ba4ea54
Adam Zammit <adam.zammit@acspri.org.au>
2018-08-17 16:01:16 +1000

    Fix typo

--- views/site/index.php
+++ views/site/index.php
@@ -14,7 +14,7 @@ $this->title = 'Web frontend for QAMyData Tool';
 
 <?php $form = ActiveForm::begin(['options' => ['enctype' => 'multipart/form-data']]) ?>
 
-    <?= $form->field($model, 'imageFile')->fileInput() ?>
+    <?= $form->field($model, 'file')->fileInput() ?>
 
     <button class="btn btn-lg btn-success">Upload</button>
 
